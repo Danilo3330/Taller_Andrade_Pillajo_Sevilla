2afe673152a0282ec2c45190c55d382c01454bb6
Usuario <Usuario@DESKTOP-PML2FA6>
2022-07-14 19:24:30 -0500

    "version 1.0 Taller_Andrade_Pillajo_Sevilla 14/07/2022"

---
+++ src/ec/edu/intsuperior/modelo/Persona.java
@@ -0,0 +1,16 @@
+package ec.edu.intsuperior.modelo;
+
+import java.util.Date;
+
+public class Persona {
+String ci;
+String nombre1;
+String nombre2;
+String apellido1;
+String apellido2;
+String dirección;
+Date fechaNacimiento;
+String correoElectronico;
+
+
+}

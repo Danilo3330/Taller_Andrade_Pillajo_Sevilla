9345d8bf9edc7d573708beff8eb8ae4ee09a4c7d
Usuario <Usuario@DESKTOP-PML2FA6>
2022-07-14 20:09:15 -0500

    "version 1.1 Taller_Andrade_Pillajo_Sevilla 14/07/2022"

--- src/ec/edu/intsuperior/vista/Aplicacion.java
+++ src/ec/edu/intsuperior/vista/Aplicacion.java
@@ -1,14 +1,20 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package ec.edu.intsuperior.vista;
 
-/**
- *
- * @author Usuario
- */
+import ec.edu.intsuperior.modelo.Persona;
+import java.util.Date;
+
 public class Aplicacion {
+    public static void main(String[] args) {
+    Persona p1= new Persona();
+    Date fec= new Date();
+    fec.setYear(2003);
+    fec.setMonth(12);
+    fec.setDate(26);
+    Persona p2= new Persona("1750796250","Anthony","Danilo","Sevilla","Segura","Turubamba Bajo", fec, "sevillaa847@gmail.com");
+       System.out.println("Fecha de nacimiento; " + p2.getFechaNacimiento().getYear()+ "/" + p2.getFechaNacimiento().getMonth()+"/"+p2.getFechaNacimiento().getDate());
+       
+    }
+    
     
 }
